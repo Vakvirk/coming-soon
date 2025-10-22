84f3f33bf61db27b0b1e1931f7a54870f603d9a6
Bartosz Lewandowicz <bartosz.lewandowicz@gmail.com>
2025-10-22 13:30:37 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import ComingSoon from "./components/ComingSoon";
+
+function App() {
+  return (
+    <>
+      <div>
+        <ComingSoon></ComingSoon>
+      </div>
+    </>
+  );
+}
+
+export default App;
